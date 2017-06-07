32ebffc34e2f5deb9c993cf754129a574415279f
viktor <baranov.viktor.27@gmail.com>
2017-06-07 14:43:54 +0300

    Key class of Oracles contract. Initial commit

---
+++ src/KeyClass.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.11;
+
+contract KeyClass {
+    struct InitialKey {
+        bool isNew;
+    }
+   
+    struct MiningKey {
+        bool isActive;
+    }
+    
+    struct PayoutKey {
+        bool isActive;
+    }
+
+    struct VotingKey {
+        bool isActive;
+    }
+    
+    mapping(address => MiningKey) public miningKeys;
+    mapping(address => PayoutKey) public payoutKeys;
+    mapping(address => VotingKey) public votingKeys;
+    mapping(address => InitialKey) public initialKeys;
+}
